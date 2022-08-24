7f4357330e8e9dd591ca3c22462e3fe2b37e14be
jianjianjianjianjianjian <111936280+jianjianjianjianjianjian@users.noreply.github.com>
2022-08-24 11:02:21 +0800

    Update fff.py

--- fff.py
+++ fff.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-#My First App
 st.header("My first Streamlit App")
 st.write(pd.DataFrame({
     'Intplan': ['yes', 'yes', 'yes', 'no'],
